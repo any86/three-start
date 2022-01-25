6dc17121ae85deff472475fe72546e358f4d57fa
Russell <383514580@qq.com>
2022-01-25 13:14:10 +0800

    第二天

--- src/three.hook.ts
+++ src/three.hook.ts
@@ -1,9 +1,19 @@
 import * as THREE from "three";
 import type { Mesh, Object3D } from "three";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import {DragControls} from 'three/examples/jsm/controls/DragControls';
 import { type Ref } from 'vue';
+
+
+
 export default function (wrapRef: Ref<HTMLElement>) {
   const scene = new THREE.Scene();
+
+
+  const helper = new THREE.GridHelper(10)
+  scene.add(helper)
+  helper.rotateZ(90/180*Math.PI)
+
 
   // 光源
   let spotLight = new THREE.SpotLight(0xffffff);
@@ -13,8 +23,8 @@ export default function (wrapRef: Ref<HTMLElement>) {
   const ambientLight = new THREE.AmbientLight(0x333333);
   scene.add(ambientLight);
 
-  const plane = createPlane();
-  scene.add(plane);
+  // const plane = createPlane();
+  // scene.add(plane);
 
 
   function createPlane({ color = '#ccc' } = {}) {
@@ -27,20 +37,6 @@ export default function (wrapRef: Ref<HTMLElement>) {
     return planeMesh;
   };
 
-  let mesh3: Object3D;
-
-  const loader = new GLTFLoader();
-  loader.load('/1.glb', function (gltf) {
-    mesh3 = gltf.scene.children[3];
-
-    console.log(gltf.scene.children);
-    scene.add(gltf.scene);
-    mesh3.rotation.set(10, 10, 10);
-
-  }, undefined, function (error) {
-    console.error(error);
-  });
-
 
   const camera = new THREE.PerspectiveCamera(
     45,
@@ -48,6 +44,45 @@ export default function (wrapRef: Ref<HTMLElement>) {
     0.1,
     100
   );
+  camera.lookAt(0,0,0);
+
+
+  let mesh3: Object3D;
+
+  const loader = new GLTFLoader();
+  loader.load('/1.glb', function (gltf) {
+    console.log(gltf.scene);
+    mesh3 = gltf.scene.children[2];
+    scene.add(gltf.scene);
+    mesh3.rotation.set(10, 10, 10);
+
+
+
+    const controls = new DragControls( [mesh3], camera, renderer.domElement );
+
+    // add event listener to highlight dragged objects
+    
+    controls.addEventListener( 'dragstart', function ( event ) {
+    console.log();
+      event.object.material.emissive.set( 0xaaaaaa );
+    console.log(event);
+    } );
+    
+    controls.addEventListener( 'dragend', function ( event ) {
+    
+      event.object.material.emissive.set( 0x000000 );
+    
+    } );
+
+
+
+
+  }, undefined, function (error) {
+    console.error(error);
+  });
+
+
+
 
 
 
@@ -64,19 +99,28 @@ export default function (wrapRef: Ref<HTMLElement>) {
   camera.position.z = 15;
 
 
+
+
+
+
+
   function animate() {
     requestAnimationFrame(animate);
-    if (mesh3) {
-      mesh3.rotation.x += 0.01;
-      mesh3.rotation.y += 0.01;
-      if (mesh3.scale.x > 3) {
-        mesh3.scale.x -= 0.01;
-        mesh3.scale.y -= 0.01;
-      } else {
-        mesh3.scale.x += 0.01;
-        mesh3.scale.y += 0.01;
-      }
-    }
+    // if (mesh3) {
+    //   mesh3.rotation.x += 0.01;
+    //   mesh3.rotation.y += 0.01;
+    //   if (mesh3.scale.x > 3) {
+    //     mesh3.scale.x -= 0.01;
+    //     mesh3.scale.y -= 0.01;
+    //     mesh3.scale.z -= 0.01;
+
+    //   } else {
+    //     mesh3.scale.x += 0.01;
+    //     mesh3.scale.y += 0.01;
+    //     mesh3.scale.z += 0.01;
+
+    //   }
+    // }
     renderer.render(scene, camera);
   }
 
